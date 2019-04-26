d67294bd38627b7db541fdeca2d0e03e1ffbefde
Autumnalis <carlafeng.cfah@gmail.com>
2019-04-26 20:22:15 +0200

    CSS added

--- 01.js
+++ 01.js
@@ -1,8 +1,17 @@
 const app = new Vue({
     el: '#app',
     data: {
-        titulo: 'Hello world',
-        flowers: ['Girasol', 'Rosa', 'Jazmin']
+        title: 'ASDFG', 
+        flower: [
+            {name:'Sunflower', quantity: 10}, 
+            {name:'Rose', quantity: 9}, 
+            {name:'Daisy', quantity: 8},
+            {name:'Jazmin', quantity: 7},
+            {name:'bla', quantity: 6},
+            {name:'ble', quantity: 5},
+            {name:'bli', quantity: 4},
+            {name:'blo', quantity: 3},
+            {name:'Violet', quantity: 1}]
     }
 
 
