e4aa176a63d0ae9b99f34f45d4f909c5bbb6870d
Autumnalis <carlafeng.cfah@gmail.com>
2019-04-29 14:15:44 +0200

    Something

--- 01.js
+++ 01.js
@@ -1,7 +1,7 @@
 const app = new Vue({
     el: '#app',
     data: {
-        title: 'ASDFG', 
+        title: 'TRIALS WEE', 
         flower: [
             {name:'Sunflower', quantity: 10}, 
             {name:'Rose', quantity: 9}, 
@@ -9,10 +9,28 @@ const app = new Vue({
             {name:'Jazmin', quantity: 7},
             {name:'bla', quantity: 6},
             {name:'ble', quantity: 5},
-            {name:'bli', quantity: 4},
-            {name:'blo', quantity: 3},
             {name:'Violet', quantity: 1}
-        ]
+        ],
+
+        newFlower: "", 
+        //newQuantity: ""
+    },
+
+    methods:{
+        addFlower(){
+            this.flower.push({
+                name: this.newFlower,
+                quantity: 0
+            })
+            console.log('Click a');
+        },
+        /* addQuantity(){
+            this.quantity.push({
+                quantity: this.newQuantity
+            })
+            console.log('Click b')
+        }*/
+
     }
 
 
