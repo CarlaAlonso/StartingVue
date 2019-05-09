0f1e03daee78de87b576b9ad1bbbe99a90d5946a
Autumnalis <carlafeng.cfah@gmail.com>
2019-05-09 14:52:27 +0200

    it works

--- 01.js
+++ 01.js
@@ -1,37 +1,45 @@
 const app = new Vue({
     el: '#app',
     data: {
-        title: 'TRIALS WEE', 
+        title: 'TRIALS WEE',
         flower: [
-            {name:'Sunflower', quantity: 10}, 
-            {name:'Rose', quantity: 9}, 
-            {name:'Daisy', quantity: 8},
-            {name:'Jazmin', quantity: 7},
-            {name:'bla', quantity: 6},
-            {name:'ble', quantity: 5},
-            {name:'Violet', quantity: 1}
+            { name: 'Sunflower', quantity: 10 },
+            { name: 'Rose', quantity: 9 },
+            { name: 'Daisy', quantity: 8 },
+            { name: 'Violet', quantity: 1 }
         ],
-
-        newFlower: "", 
+        newFlower: "",
         //newQuantity: ""
+        //total: 0
     },
-
-    methods:{
-        addFlower(){
+    methods: {
+        addFlower() {
             this.flower.push({
                 name: this.newFlower,
                 quantity: 0
             })
-            console.log('Click a');
+            //console.log('Click a');
+
+            this.addFlower = '';
         },
-        /* addQuantity(){
-            this.quantity.push({
-                quantity: this.newQuantity
-            })
-            console.log('Click b')
-        }*/
 
+    },
+    computed: {
+        plusFlower() {
+            this.total = 0;
+            for (flower of this.flower) { //Cada vez que haya un cambio en "cantidad" se modificará lo siguiente
+                this.total = this.total + flower.quantity;
+            }
+            return this.total;
+        }
     }
-
-
 })
+
+
+/* En "methods" después de addFlower
+addQuantity(){
+    this.quantity.push({
+        quantity: this.newQuantity
+    })
+    console.log('Click b')
+}*/
